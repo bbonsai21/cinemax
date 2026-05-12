4937dd33f85e049cab504c52ec1fd4878800b151
Doppelblitz <contact.enquirer648@passinbox.com>
2026-05-12 16:46:05 +0200

    feat(ValidationException): generic exception class for checked validation

---
+++ src/cinemax/exception/ValidationException.java
@@ -0,0 +1,16 @@
+package exception;
+
+import model.ValidationError;
+
+public class ValidationException extends Exception {
+        private final ValidationError error;
+
+        public ValidationException(ValidationError error) {
+                super( ValidationError.getMessageKey( error ) );
+
+                this.error = error;
+        }
+
+        public ValidationError getError()
+        { return this.error; }
+}
