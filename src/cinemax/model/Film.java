97a7690a64e546f390efb50ede967f95dfadd65e
Doppelblitz <contact.enquirer648@passinbox.com>
2026-05-12 16:48:30 +0200

    feat(Film): base class for storing Film data in memory

---
+++ src/cinemax/model/Film.java
@@ -0,0 +1,25 @@
+package model;
+import exception.ValidationException;
+
+public class Film {
+        public final String name;
+        public final int year, length, minAge;
+
+        Film( String name, int year, int length, int minAge ) throws ValidationException {
+                if ( name == null || name.isBlank() )
+                        throw new ValidationException( ValidationError.FILM_TITLE_EMPTY );
+
+                if ( year < ( int )ValidationError.FILM_YEAR_INVALID.getParams()[0] ||
+                        year > ( int )ValidationError.FILM_YEAR_INVALID.getParams()[1] )
+                        throw new ValidationException( ValidationError.FILM_YEAR_INVALID );
+
+                if ( length < 0 ) throw new ValidationException( ValidationError.FILM_LENGTH_NEGATIVE );
+
+                if ( minAge < 0 ) throw new ValidationException( ValidationError.FILM_MIN_AGE_NEGATIVE );
+
+                this.name = name;
+                this.year = year;
+                this.length = length;
+                this.minAge = minAge;
+        }
+}
