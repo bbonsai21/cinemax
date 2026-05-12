176f87cb3b93fd5c24e7390ef12e0869f6495581
Doppelblitz <contact.enquirer648@passinbox.com>
2026-05-12 16:44:39 +0200

    feat(ValidationError): enum class for retrieving error messages in i18n, with parameters

---
+++ src/cinemax/model/ValidationError.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.Objects;
+
+public enum ValidationError {
+        FILM_TITLE_EMPTY( "error.film.title.empty" ),
+        FILM_YEAR_INVALID( "error.film.year.invalid", 1850, 2100 ),
+        FILM_LENGTH_NEGATIVE( "error.film.length.negative" ),
+        FILM_MIN_AGE_NEGATIVE( "error.film.minAge.negative" );
+
+        private final String msgKey;
+        private final Object[] params;
+
+        ValidationError( String msgKey, Object... params )
+        {
+                this.msgKey = msgKey;
+
+                // creating a variable-size list of params instead of choosing a precise set of
+                // finite amount params since this way it's easier to expand the code and allow
+                // the introduction of new params wherever they're needed
+                this.params = params;
+        }
+
+        public static String getMessageKey( ValidationError error ) {
+                Objects.nonNull( error );
+
+                return error.msgKey;
+        }
+
+        public Object[] getParams()
+        { return params; }
+}
