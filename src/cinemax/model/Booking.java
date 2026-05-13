ca978cefba57dfc9f76a0fe496701028e218e849
Doppelblitz <contact.enquirer648@passinbox.com>
2026-05-13 16:57:19 +0200

    feat(Booking): finished base constructor

--- src/cinemax/model/Booking.java
+++ src/cinemax/model/Booking.java
@@ -1,9 +1,21 @@
 package model;
 
+import java.util.Objects;
+
 import model.SeatMap.Seat;
 
+/**
+ * Class meant to store raw booking data in memory.
+ */
 public final class Booking {
-        public Booking( Projection projection, Seat seats[] ) {
-                
+        private final Projection projection;
+        private final Seat[] seats;
+
+        public Booking( Projection projection, Seat[] seats ) {
+                Objects.requireNonNull( projection );
+                Objects.requireNonNull( seats );
+
+                this.projection = projection;
+                this.seats = seats;
         }
 }
