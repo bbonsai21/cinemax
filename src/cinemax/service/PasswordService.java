877f5e5afbf894878dc0580eee142ce91eb3f701
Doppelblitz <contact.enquirer648@passinbox.com>
2026-05-23 01:53:40 +0200

    fix: refactored. Updated to newest standards

--- src/cinemax/service/PasswordService.java
+++ src/cinemax/service/PasswordService.java
@@ -1,10 +1,16 @@
 package service;
 
+import java.util.Base64;
 import java.util.Objects;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
 /**
  * This class is meant to create secure hashes, starting from plain text.
@@ -16,92 +22,46 @@ public class PasswordService {
 	
 	private static String getSalt()
 	{
-		SecureRandom sr; 
-		try {
-			sr = SecureRandom.getInstance( "SHA1PRNG" );
-		} catch( NoSuchAlgorithmException e ) { throw new RuntimeException( e.getMessage() ); }
+		SecureRandom sr = new SecureRandom();
+		byte[] bytes = new byte[16];
+		sr.nextBytes( bytes );
 
-		byte[] salt = new byte[16];
-		sr.nextBytes( salt );
-
-		return salt.toString();
+		return Base64.getEncoder().encodeToString( bytes );
 	}
 
 	/**
-	 * Creates and returns a secure hash, given a plain string password and a salt.
-	 * @param password plain text string to hash
-	 * @param salt salt string
+	 * Returns the hashed password, containing also the hash, in itself.
+	 * @param password plain text to hash
+	 * @param salt
 	 * @return String hashed password
-	 * @see #getHash(String)
-	 * @see #getHashAndSalt(String)
 	 */
 	public static String getHash( String password, String salt )
 	{
 		Objects.requireNonNull( password );
 		Objects.requireNonNull( salt );
 
-		MessageDigest md;
-		try {
-			md = MessageDigest.getInstance( "SHA-256" );
-			md.update( salt.getBytes() );
-			byte[] bytes = md.digest( password.getBytes() );
+		int iterations = 310000; // 310k
 
-			StringBuilder sb = new StringBuilder();
-			for ( int i = 0; i < bytes.length; i++ )
-			{
-				sb.append( Integer.toString( ( bytes[i] & 0xff ) + 0x100, 16 ).substring( 1 ) );
-			}
+		PBEKeySpec spec = new PBEKeySpec( password.toCharArray(), salt.getBytes( StandardCharsets.UTF_8 ), iterations, 8*64 );
+		SecretKeyFactory skf;
+		try { skf = SecretKeyFactory.getInstance( "PBKDF2WithHmacSHA256" ); }
+		catch( NoSuchAlgorithmException e ) { throw new RuntimeException( e.getMessage() ); }
 
-			return sb.toString();
-		}
-		catch( NoSuchAlgorithmException e )
-		{ throw new RuntimeException( e.getMessage() ); }
+		byte[] hash;
+		try { hash = skf.generateSecret( spec ).getEncoded(); }
+		catch( InvalidKeySpecException e ) { throw new RuntimeException( e.getMessage() ); }
+
+		return Integer.toString( iterations ) + ":" + Base64.getEncoder().encodeToString( hash ) + ":" + salt;
 	}
 
-	/**
-	 * Creates and returns a secure hash and a salt, given a plain string password.
-	 * @param password plain text string to hash
-	 * @return String hashed password
-	 * @see #getHashAndSalt(String)
-	 * @see #getHash(String, String)
-	 */
 	public static String getHash( String password )
 	{
-		Objects.requireNonNull( password );
-
 		return getHash( password, getSalt() );
 	}
 
-	/**
-	 * Creates and returns an array of 2 strings, containing the provided password hashed, and the salt used to hash it.
-	 * @param password plain text string to hash
-	 * @return String[] array of 2 strings
-	 * @see #getHash(String)
-	 * @see #getHash(String, String)
-	 */
-	public static String[] getHashAndSalt( String password )
+	public static boolean compareStringToHash( String plainText, String hash )
 	{
-		Objects.requireNonNull( password );
-
-		String[] res = new String[2];
-
-		String salt = getSalt();
-
-		res[0] = getHash( password, salt );
-		res[1] = salt;
-
-		return res;
-	}
-
-	/**
-	 * Checks if a plain text password coresponds to a hashed password, given the salt
-	 * @param password plain text password
-	 * @param hashedPassword hashed password to check password against
-	 * @param salt salt of the hashed password
-	 * @return
-	 */
-	public static boolean checkPasswordCorespondence( String password, String hashedPassword, String salt )
-	{
-		return ( getHash( password, salt ) == hashedPassword );
+		String salt = hash.split( ":" )[2];
+		return MessageDigest.isEqual( getHash( plainText, salt ).getBytes( StandardCharsets.UTF_8 ), hash.getBytes( StandardCharsets.UTF_8 ) );
 	}
 }
