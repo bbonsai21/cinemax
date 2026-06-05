dad407d02781419deff56d2da2a10ae3bd571c11
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-05 13:52:23 +0200

    doc: addded javadoc comments

--- src/cinemax/tui/UserMenu/UserMenu.java
+++ src/cinemax/tui/UserMenu/UserMenu.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 import tui.MenuEntry;
 
+/**
+ * Abstract representation of a user menu.
+ */
 public abstract class UserMenu implements Runnable {
     protected final ArrayList<MenuEntry> entries;
     
@@ -11,6 +14,10 @@ public abstract class UserMenu implements Runnable {
         entries = buildEntries();
     }
 
+    /**
+     * Builds the entries the class can make use of
+     * @return ArrayList<MenuEntry> list of entries
+     */
     protected abstract ArrayList<MenuEntry> buildEntries();
 
     public abstract void run();
