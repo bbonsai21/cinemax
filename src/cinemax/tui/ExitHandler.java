24a5c864e46b4f08793c78431d3a4c3202318981
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-05 18:16:35 +0200

    feat(ExitHandler): added

---
+++ src/cinemax/tui/ExitHandler.java
@@ -0,0 +1,24 @@
+package tui;
+
+/**
+ * Handles application exit explicitly and gracefully.
+ */
+public class ExitHandler implements Runnable {
+    private ExitHandler() { }
+
+    /**
+     * Prompts the user to exit. If positive, closes application.
+     */
+    public static void promptAndExit() {
+        String input = Input.INSTANCE.readLine( "exit.prompt" );
+        if ( input.toLowerCase().strip().equals( Message.get( "exit.confirm.yes" ).toLowerCase().strip() ) ) {
+            IO.println( "Au revoir!" );
+            System.exit( 0 );
+        }
+    }
+
+    @Override
+    public void run() {
+        promptAndExit();
+    }
+}
