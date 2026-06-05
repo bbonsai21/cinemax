3c382a4ed5da5ce4615808fdd2cd76eb252f06e7
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-05 15:51:14 +0200

    chore: removed String constructor parameter and turned into unchekced exception

--- src/cinemax/tui/LogoutException.java
+++ src/cinemax/tui/LogoutException.java
@@ -3,8 +3,8 @@ package tui;
 /**
  * Exception thrown to easily manage the user menu flow, and log the user out.
  */
-public class LogoutException extends Exception {
-    public LogoutException( String key ) {
+public class LogoutException extends RuntimeException {
+    public LogoutException() {
         super( null, null, true, false );
     }
 }
