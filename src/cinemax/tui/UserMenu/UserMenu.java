6b53eb80e547e92d0b5c659b5a75667861ff3466
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-06 12:32:59 +0200

    fix(changeLanguage): used 'equals' method instead of '=='

--- src/cinemax/tui/UserMenu/UserMenu.java
+++ src/cinemax/tui/UserMenu/UserMenu.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import exception.LogoutException;
 import service.AuthService;
+import tui.Displayer;
+import tui.ExitHandler;
 import tui.Input;
 import tui.MenuEntry;
 import tui.Message;
@@ -13,7 +15,7 @@ import tui.Message;
  * Abstract representation of a user menu.
  */
 public abstract class UserMenu implements Runnable {
-    protected final ArrayList<MenuEntry> entries;
+    protected ArrayList<MenuEntry> entries;
 
     protected UserMenu() {
         entries = buildEntries();
@@ -28,23 +30,56 @@ public abstract class UserMenu implements Runnable {
 
     public abstract void run();
 
+    public void rebuildEntries() {
+        this.entries = buildEntries();
+    }
+
     private void changeLanguage() {
+        Displayer.cleanScreen();
+
+        Displayer.title(Message.get("menu.language.change"));
+
         var languages = new ArrayList<>(List.of(
                 new MenuEntry(Message.get("menu.return"), null),
                 new MenuEntry("EN", null),
                 new MenuEntry("IT", null)));
 
-        MenuEntry choice = Input.INSTANCE.choose(languages);
+        MenuEntry choice = Input.choose(languages);
 
-        if (choice.labelKey() == "menu.return")
+        if (choice.labelKey().equals("menu.return"))
             return;
 
         Message.setLanguage(choice.labelKey().toLowerCase());
+
+        rebuildEntries();
+    }
+
+    private void displayExtraInfo() {
+        Displayer.cleanScreen();
+
+        Displayer.title("Cinemax");
+        Displayer.section(Message.get("menu.extra.section.credits"));
+        Displayer.body(Message.get("menu.extra.developer", "https://github.com/bbonsai21"));
+
+        Displayer.body(Message.get("message.await.enter"));
+        Input.awaitInput();
+
+        Displayer.cleanScreen();
+
+        Displayer.title("Cinemax");
+        Displayer.section(Message.get("menu.extra.section.license"));
+        Displayer.fromFile("LICENSE.txt",
+                "There was an error displaying the license.\nYou can still check it in the project's root folder file LICENSE.txt");
+
+        Displayer.body(Message.get("message.await.enter"));
+        Input.awaitInput();
     }
 
     protected ArrayList<MenuEntry> getSharedEntries() {
         return new ArrayList<>(List.of(
-                new MenuEntry("language.change", this::changeLanguage)));
+                new MenuEntry(Message.get("menu.language.change"), this::changeLanguage),
+                new MenuEntry(Message.get("menu.extra"), this::displayExtraInfo),
+                new MenuEntry(Message.get("menu.quit"), ExitHandler::promptAndExit)));
     }
 
     protected ArrayList<MenuEntry> getSharedLoggedEntries() {
