a64a4d92bb2462c49e6ef9e63d077633d1724072
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-07 21:42:22 +0200

    feat(getPath): retrieving Path objects

--- src/cinemax/repository/FilePaths.java
+++ src/cinemax/repository/FilePaths.java
@@ -1,15 +1,34 @@
 package repository;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public enum FilePaths {
-	PROJECTIONS( "data/proiezioni.csv" ),
-	USERS( "data/users.csv" ),
-	USER_BOOKINGS( "data/user_bookings.csv" );
+	PROJECTIONS("proiezioni.csv"),
+	USERS("users.csv"),
+	USER_BOOKINGS("user_bookings.csv");
 
-	private String path;
-	
-	FilePaths( String path )
-	{ this.path = path; }
+	private final String filename;
 
-	public String getPath()
-	{ return this.path; }
+	FilePaths(String filename) {
+		this.filename = filename;
+	}
+
+	public String getPath() {
+		try {
+			Path jarDir = Paths.get(
+					FilePaths.class
+							.getProtectionDomain()
+							.getCodeSource()
+							.getLocation()
+							.toURI())
+					.getParent();
+
+			return jarDir.resolve("../../data/" + filename)
+					.normalize()
+					.toString();
+		} catch (Exception e) {
+			return "data/" + filename;
+		}
+	}
 }
