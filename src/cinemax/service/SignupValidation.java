65f1b247bda3aa9626ac2fd59ef20efce25cc5e9
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-07 11:27:02 +0200

    feat(SingupValidation): added username validity

--- src/cinemax/service/SignupValidation.java
+++ src/cinemax/service/SignupValidation.java
@@ -2,13 +2,14 @@ package service;
 
 public record SignupValidation(
         boolean usernameAvailable,
+        boolean usernameValid,
         boolean passwordLongEnough,
         boolean passwordHasUppercase,
         boolean passwordHasDigit,
         boolean passwordHasSpecial,
         boolean passwordOnlyAllowedSpecial) {
     public boolean isValid() {
-        return usernameAvailable && passwordLongEnough
+        return usernameAvailable && usernameValid && passwordLongEnough
                 && passwordHasUppercase && passwordHasDigit
                 && passwordHasSpecial && passwordOnlyAllowedSpecial;
     }
