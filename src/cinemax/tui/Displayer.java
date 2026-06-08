74e0f1e8ec826d52a82197763aca575db0e4e3a9
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-09 01:21:29 +0200

    feat(fromFile): added fail message in catch block

--- src/cinemax/tui/Displayer.java
+++ src/cinemax/tui/Displayer.java
@@ -1,8 +1,5 @@
 package tui;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -227,6 +224,8 @@ public final class Displayer {
         try {
             String content = Files.readString( Paths.get( path ), StandardCharsets.UTF_8 );
             body( content );
-        } catch ( Exception e ) {}
+        } catch ( Exception e ) {
+            body( failMsg );
+        }
     }
 }
