21e9a25cbf8024ea73f2e3c41a741272d9d89995
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-09 22:32:52 +0200

    feat: removed setPath and integrated it in file operations

--- src/cinemax/repository/CsvReader.java
+++ src/cinemax/repository/CsvReader.java
@@ -19,62 +19,50 @@ import model.ParsingError;
 /**
  * Parser class for .csv files.
  */
-public final class CsvReader {
-        private static CsvReader self;
-        private static String path;
-		private static CsvProcessor processor;
+public enum CsvReader {
+	INSTANCE;
 
-        private CsvReader() {}
+	private static CsvProcessor processor;
 
-        /**
-         * Creates and returns a unique static new CsvParser object instance.
-         * @return CsvReader object instance.
-         */
-        public static CsvReader init() {
-                if ( self != null ) return self;
-
-                self = new CsvReader();
-		path = "";
-
-                return self;
-        }
+	CsvReader() {
+	}
 
 	/**
-	 * Processes the file from the set path with the set processor. Uses UTF-8 as standard charset.
+	 * Processes the file from the set path with the set processor. Uses UTF-8 as
+	 * standard charset.
+	 * 
 	 * @throws ParsingException
 	 * @see #setPath(String)
 	 * @see #setProcessor(CsvProcessor)
 	 */
-	public boolean process() throws ParsingException
-	{
+	public boolean process(String path) throws ParsingException {
 		// UTF-8 will suffice, even with movie names using non-latin alphabet letters
-		try( Stream<String> lines = Files.lines( Paths.get( path ), StandardCharsets.UTF_8 ) )
-		{
-			Optional<String> result = lines.filter( line -> processor.process( line ) )
-							.findFirst();
+		try (Stream<String> lines = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
+			Optional<String> result = lines.filter(line -> processor.process(line))
+					.findFirst();
 
 			return result.isPresent();
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			switch (e) {
-				case FileNotFoundException _ -> throw new ParsingException( ParsingError.CSVPARSER_FILE_INVALID );
-				case EOFException _ -> throw new ParsingException( ParsingError.CSVPARSER_FILE_EOF );
-				case CharacterCodingException _ -> throw new ParsingException( ParsingError.CSVPARSER_FILE_CHARCODING_INVALID );
-				case CharConversionException _ -> throw new ParsingException( ParsingError.CSVPARSER_FILE_CHARCONVERSION_INVALID );
-				default -> throw new ParsingException( ParsingError.CSVPARSER_IOEXCEPTION );
+				case FileNotFoundException _ -> throw new ParsingException(ParsingError.CSVPARSER_FILE_INVALID);
+				case EOFException _ -> throw new ParsingException(ParsingError.CSVPARSER_FILE_EOF);
+				case CharacterCodingException _ ->
+					throw new ParsingException(ParsingError.CSVPARSER_FILE_CHARCODING_INVALID);
+				case CharConversionException _ ->
+					throw new ParsingException(ParsingError.CSVPARSER_FILE_CHARCONVERSION_INVALID);
+				default -> throw new ParsingException(ParsingError.CSVPARSER_IOEXCEPTION);
 			}
 		}
 	}
 
-	public void setPath( String fpath )
-	{
-		Objects.requireNonNull( fpath );
-		path = fpath;
+	public void setProcessor(CsvProcessor csvProcessor) {
+		Objects.requireNonNull(csvProcessor);
+
+		processor = csvProcessor;
 	}
 
-	public void setProcessor( CsvProcessor csvProcessor )
-	{
-		Objects.requireNonNull( csvProcessor );
-		processor = csvProcessor;
+	public boolean writeFile(String path) {
+		Objects.requireNonNull(path);
+		return true;
 	}
 }
