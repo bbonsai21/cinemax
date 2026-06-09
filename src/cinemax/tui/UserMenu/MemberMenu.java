7f068d052bb6f64a29c8bb33a9f22ce4eb85820d
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-09 22:27:52 +0200

    feat(run, buildEntries): running with the right display; building complete actions list

--- src/cinemax/tui/UserMenu/MemberMenu.java
+++ src/cinemax/tui/UserMenu/MemberMenu.java
@@ -1,20 +1,36 @@
 package tui.UserMenu;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import model.user.User;
+import tui.Displayer;
+import tui.Input;
 import tui.MenuEntry;
 
-public class MemberMenu extends UserMenu  {
-    public MemberMenu()
-    { super(); }
+public final class MemberMenu extends LoggedUserMenu {
+    public MemberMenu(User user) {
+        super(user);
+    }
 
     protected ArrayList<MenuEntry> buildEntries() {
-        ArrayList<MenuEntry> entries = new ArrayList<>();
-        
+        ArrayList<MenuEntry> entries = getSharedEntries();
+        entries.addAll(getSharedLoggedEntries());
+        entries.addAll(new ArrayList<>(List.of(
+            
+        )));
+
         return entries;
     }
 
     public void run() {
+        while (true) {
+            Displayer.cleanScreen();
 
+            Displayer.title("MEMBER");
+
+            MenuEntry choice = Input.choose(this.entries);
+            choice.action().run();
+        }
     }
 }
