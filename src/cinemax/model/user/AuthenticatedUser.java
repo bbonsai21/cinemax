ba8a89542ca28d43181a712fcf060efeba4959fb
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-09 22:29:34 +0200

    feat(AuthenticatedUser): removed hashPass, added name, surname, domicile

--- src/cinemax/model/user/AuthenticatedUser.java
+++ src/cinemax/model/user/AuthenticatedUser.java
@@ -1,19 +1,43 @@
 package model.user;
 
+import java.util.Objects;
+
 /**
- * Class provides ulterior essential fields and methods every authenticated user must have, distinguishing them from Guest
+ * Class provides ulterior essential fields and methods every authenticated user
+ * must have, distinguishing them from Guest
  */
 public sealed abstract class AuthenticatedUser extends User permits Member, TicketsClerk, Projectionist {
-	private String username;
-	private String hashedPass;
+	private String username, name, surname, domicile;
 
-	public String getUsername()
-	{ return this.username; }
+	protected AuthenticatedUser(String username, String name, String surname, String domicile) {
+		Objects.requireNonNull(username);
+		Objects.requireNonNull(name);
+		Objects.requireNonNull(surname);
 
-	public String getHashedPass()
-	{ return this.hashedPass; }
+		this.username = username;
+		this.name = name;
+		this.surname = surname;
+		this.domicile = domicile;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public String getDomicile() {
+		return this.domicile;
+	}
 
 	@Override
-	public String toString()
-	{ return this.username; }
+	public String toString() {
+		return this.username;
+	}
 }
