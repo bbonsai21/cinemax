e20158acc128a2abbb2641194a815f7ee63162b2
bbonsai21 <rmastellone@studenti.uninsubria.it>
2026-06-09 22:25:27 +0200

    feat(getSharedLoggedEntries, buildEntries, logout): added

--- src/cinemax/tui/UserMenu/LoggedUserMenu.java
+++ src/cinemax/tui/UserMenu/LoggedUserMenu.java
@@ -1,11 +1,31 @@
 package tui.UserMenu;
 
-import model.user.User;
+import java.util.ArrayList;
+import java.util.List;
 
-public sealed abstract class LoggedUserMenu permits MemberMenu, ProjectionistMenu, TicketsClerkMenu {
+import exception.LogoutException;
+import model.user.User;
+import service.AuthService;
+import tui.MenuEntry;
+import tui.Message;
+
+public sealed abstract class LoggedUserMenu extends UserMenu permits MemberMenu, ProjectionistMenu, TicketsClerkMenu {
     User user;
 
     protected LoggedUserMenu(User user) {
+        super();
         this.user = user;
+    }
+
+    protected abstract ArrayList<MenuEntry> buildEntries();
+
+    protected ArrayList<MenuEntry> getSharedLoggedEntries() {
+        return new ArrayList<>(List.of(
+                new MenuEntry(Message.get("menu.logged.logout"), this::logout)));
+    }
+
+    private void logout() {
+        AuthService.INSTANCE.logout();
+        throw new LogoutException();
     }
 }
